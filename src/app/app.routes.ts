713c071ed015dfae75ee34575ca68277426b676c
geovaniRalandison <44520852+geovaniRalandison@users.noreply.github.com>
2026-09-16 13:43:25 +0200

    Add family page template, routes and family navigation button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { TaskPlannerComponent } from './components/task-planner/task-planner.component';
+import { FamilyComponent } from './components/family/family.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: TaskPlannerComponent, canActivate: [AuthGuard] },
+  { path: 'family', component: FamilyComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' }
